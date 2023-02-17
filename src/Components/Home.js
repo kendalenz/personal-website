a8f7d1f476817e9ac728fada0f5218dd8a4e5082
Kendal Enz <kendal.enz@gmail.com>
2023-02-16 23:14:02 -0500

    typo fix

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
                         <img src="/static/headshot.jpg" class='md-4' id='headshot'></img>
                     </div>
                     <div class="col-sm float-left" id='about-me-text'>
-                        <p>Hello! I'm a fullstack software engineer with an eye for design, and I love crafting beautiful websites. My background in journalsim and creative writing has given me excellent communications skills, and my work as a project manager has allowed me to develope exceptional organizational and time management skills.</p>  
+                        <p>Hello! I'm a fullstack software engineer with an eye for design, and I love crafting beautiful websites. My background in journalsim and creative writing has given me excellent communications skills, and my work as a project manager has allowed me to develop exceptional organizational and time management skills.</p>  
                         <p>In my free time, I enjoy long runs through the city, reading in the park and writing short stories. I'm based in Brooklyn, New York.</p>
                     </div>
                 </div>
