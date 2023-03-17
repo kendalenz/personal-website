295929a0e6dfd0c7038a815b87127ce7957f280f
Kendal Enz <kendal.enz@gmail.com>
2023-03-17 09:45:22 -0400

    updated competencies and cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import App from './Components/App';
+import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
 
 const root = createRoot(document.querySelector('#root'));
 
 root.render(
-    <HashRouter>
-      <App />
-    </HashRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
 
 
