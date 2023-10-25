61603d0f8a654faa81bb3f3d1f441f22268d8b4d
Kendal Enz <kendal.enz@gmail.com>
2023-10-25 17:00:27 -0400

    added narrativegenius project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,7 +5,16 @@ const Projects = () => {
       <div>
         <h3>Projects</h3>
         <div className='d-flex flex-row flex-wrap justify-content-center'>
-        <div className='card mx-4 my-4'>
+          <div className='card mx-4 my-4'>
+            <img src='../static/NarrativeGenius.png' className='card-img-top' alt='...'></img>
+              <div className='card-body'>
+                <h4 className='card-title'>NarrativeGenius</h4>
+                <p className='card-text'>A creative writing AI chatbot.</p>
+                <a href='https://narrative-genius.pages.dev/' className='btn btn-primary btn-sm project-button mx-2'>Visit site</a>
+                <a href='https://github.com/kendalenz/narrative-genius' className='btn btn-primary btn-sm project-button mx-2'>View code</a>
+              </div>
+          </div>
+          <div className='card mx-4 my-4'>
             <img src='../static/elysium.png' className='card-img-top' alt='...'></img>
               <div className='card-body'>
                 <h4 className='card-title'>Elysium</h4>
@@ -13,7 +22,7 @@ const Projects = () => {
                 <a href='https://earthen-foods.onrender.com/' className='btn btn-primary btn-sm project-button mx-2'>Visit site</a>
                 <a href='https://github.com/kendalenz/elysium' className='btn btn-primary btn-sm project-button mx-2'>View code</a>
               </div>
-           </div>
+          </div>
           <div className='card mx-4 my-4'>
             <img src='../static/EvoGym.png' className='card-img-top' alt='...'></img>
               <div className='card-body'>
@@ -22,7 +31,7 @@ const Projects = () => {
                 <a href='https://fitness-app-ewf.pages.dev/' className='btn btn-primary btn-sm project-button mx-2'>Visit site</a>
                 <a href='https://github.com/kendalenz/fitness-app' className='btn btn-primary btn-sm project-button mx-2'>View code</a>
               </div>
-           </div>
+          </div>
            <div className='card mx-4 my-4'>
             <img src='../static/gabriel-zapata.png' className='card-img-top mb-2' alt='...'></img>
             <div className='card-body'>
